9ce99fbc252f5f1ac093d563ee8385b9cf45ee4a
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-04-03 11:51:17 -0500

    CRU API routes functional for Thoughts, but Delete returns null when the request is made.

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -5,14 +5,45 @@ const thoughtController = {
     // /api/thoughts
 
         // get all thoughts
-
+            getAllThoughts(req, res) {
+                Thought.find({})
+                .then(dbThoughtData => res.json(dbThoughtData))
+                .catch(err => {
+                    console.log(err)
+                    res.sendStatus(400)
+                })
+            },
         // get single thought by _id
+            getThoughtById({ params }, res){
+                Thought.findOne({ _id: params.id})
+                .then(dbThoughtData => res.json(dbThoughtData))
+                .catch(err => res.json(err))
+            },
 
         // create a new thought (push created thought's id to associated user's thoughts array field)
-
+            createThought({ body }, res){ 
+                Thought.create(body)
+                .then(dbThoughtData => res.json(dbThoughtData))
+                .catch(err => res.json(err))
+            },
         // update a thought by _id
-
+        updateThought({ params, body }, res) {
+           Thought.findOneAndUpdate({ _id: params.id}, body, { new: true})
+            .then(dbThoughtData => {
+                if(!dbThoughtData) {
+                    res.status(404).json({ message: "No Thought found with this id!"})
+                    return;
+                }
+                res.json(dbThoughtData)
+            })
+            .catch(err => res.json(err))
+        },
         // remove a thought by _id
+        deleteThought({ params }, res) {
+            User.findOneAndDelete({ _id: params.id })
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => res.json(err))
+        }
 
     // /api/thoughts/:thoughtId/reactions
 
@@ -20,3 +51,5 @@ const thoughtController = {
 
         // delete to pull and remove a reaction by reaction's reactionId value
 }
+
+module.exports = thoughtController;
--- models/Thought.js
+++ models/Thought.js
@@ -1,32 +1,6 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const ThoughtSchema = new Schema({
-    thoughtText: {
-        type: String,
-        required: 'Thought must contain text!',
-        minLength: 1,
-        maxlength: 280
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => dateFormat(createdAtVal)
-    },
-    username: {
-        // user that created this thought 
-        type: String,
-        required: true
-    }
-},
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
-        },
-        id: false
-    }
-);
 
 const ReactionSchema = new Schema({
     reactionId: {
@@ -55,6 +29,34 @@ const ReactionSchema = new Schema({
         id: false
     }
 )
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: 'Thought must contain text!',
+        minLength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    username: {
+        // user that created this thought 
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
 
 // Thought virtual for reaction count
 ThoughtSchema.virtual('reactionCount').get(function () {
