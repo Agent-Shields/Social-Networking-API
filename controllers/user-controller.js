fc61c1d3c87aea4eb8526a9b8900e4759a3b3d93
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-04-03 01:11:27 -0500

    CRUD operations for User all working

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -13,17 +13,36 @@ const userController = {
                 });
             },
         // get single user by _id and poplated thought and friend data
+            getUserById({ params }, res) {
+                User.findOne({ _id: params.id})
+                .then(dbUserData => res.json(dbUserData))
+                .catch(err => res.json(err))
+            },
 
         // post a new user
             createUser({ body }, res) {
                 User.create(body)
                 .then(dbUserData => res.json(dbUserData))
                 .catch(err => res.json(err))
-            }
+            },
         // update a user by _id
-
+            updateUser({ params, body }, res) {
+                User.findOneAndUpdate({ _id: params.id}, body, { new: true})
+                .then(dbUserData => {
+                    if(!dbUserData) {
+                        res.status(404).json({ message: "No User found with this id!"})
+                        return;
+                    }
+                    res.json(dbUserData)
+                })
+                .catch(err => res.json(err))
+            },
         // delete a user by _id
-
+            deleteUser({ params }, res) {
+                User.findOneAndDelete({ _id: params.id })
+                .then(dbUserData => res.json(dbUserData))
+                .catch(err => res.json(err))
+            }
     // /api/users/:userId/friends/:friendId
     
         // post to add new friend to user's friend list
